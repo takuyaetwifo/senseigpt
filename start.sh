8ff8973f33c027a42ba6697d246d226f76511b6e
takuyaetwifo <59362248+takuyaetwifo@users.noreply.github.com>
2025-05-24 13:45:59 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # VOICEVOX Engine（バックグラウンド起動）
-/opt/voicevox_engine/run.sh --host 0.0.0.0 --use_gpu false &
+/opt/voicevox_engine/run --host 0.0.0.0 --use_gpu false &
 
 # Flaskアプリ（フォアグラウンド起動、必要なら GunicornにしてもOK）
 python app.py
